4e64113494fc5cdf77c77786495a7a53c1e5bc12
RafDes21 <israeltineo84@gmail.com>
2023-07-19 18:33:05 -0300

    login.html, login css, mobile terminado

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ app.use(express.static('public'))
 
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'index.html')));
 app.get('/register', (req,res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
+app.get('/login', (req,res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
+
 
 
 app.get('/home-main', (req,res) => res.sendFile(path.join(__dirname, 'views', 'partials', 'home-main.html')));
