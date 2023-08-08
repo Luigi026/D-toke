75d28244cc9805c7a3be82ecc1121fdd9b4197c5
Luigi026 <luigi.navarro026@gmail.com>
2023-08-08 14:39:13 -0300

    feat: refactorizacion con express-generator subida

---
+++ src/controllers/usersController.js
@@ -0,0 +1,11 @@
+module.exports = {
+    login: (req, res) => {
+        return res.render('login');
+    },
+    register: (req, res) => {
+      return res.render('register');
+    },
+    productCart: (req, res) => {
+      return res.render('productCart');
+    }
+}
