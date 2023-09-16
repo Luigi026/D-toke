d21f2530c419910e15a812a4e1acd8695529ce58
Caceres Matias <kcerescmh98@gmail.com>
2023-09-16 00:56:06 -0300

    feat: login validations done!

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,14 +1,39 @@
+const { validationResult } = require("express-validator")
+const { readJSON } = require("../data")
+
+
 module.exports = {
-    login: (req, res) => {
-        return res.render('login');
-    },
-    loginProcess: (req, res) => {
+  login: (req, res) => {
+    return res.render('login');
+  },
+  loginProcess: (req, res) => {
+    const errors = validationResult(req);
+
+    if (errors.isEmpty()) {
+      const users = readJSON('users.json')
+      const { id, name, role } = users.find(user => user.email === req.body.email)
+
+      /* req.session.usuario = {
+        id,
+        name,
+        role
+      } */
+
+      return res.redirect('/')
       
-    },
-    register: (req, res) => {
-      return res.render('register');
-    },
-    productCart: (req, res) => {
-      return res.render('productCart');
+    } else {
+
+      return res.render('login',{
+        errors : errors.mapped(),
+        old : req.body
+    })
     }
+
+  },
+  register: (req, res) => {
+    return res.render('register');
+  },
+  productCart: (req, res) => {
+    return res.render('productCart');
+  }
 }
