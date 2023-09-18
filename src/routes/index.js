c4bdebedff23239463809debe371ae86a983c986
Luigi026 <luigi.navarro026@gmail.com>
2023-09-18 18:05:15 -0300

    feat : "admin notUserCheck"

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
 const indexController = require('../controllers/indexController');
+const adminCheck = require('../middlewares/adminCheck');
 
 /* / */
 router.get('/', indexController.index);
 router.get('/search', indexController.search); 
+router.get('/admin', adminCheck, indexController.admin);
 
 module.exports = router;
