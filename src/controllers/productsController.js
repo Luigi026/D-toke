5dcfc4573e06f4d827ba167f358b4e2738e31fc1
israel tineo <israeltineo84@gmail.com>
2023-11-03 16:04:21 -0300

    add config in productcontroller.udpate

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -15,19 +15,19 @@ module.exports = {
   list: (req, res) => {
 
     const productsList = db.Product.findAll({
-      include : ['category'],
+      include: ['category'],
       order: ['model'],
-  })
-  const categories = db.Category.findAll();
+    })
+    const categories = db.Category.findAll();
 
-  Promise.all([productsList, categories])
-  .then(([productsList, categories]) => {
-    res.render("productsList", {
-    productsList,
-    categories,
-    toThousand
-    });
-  })
+    Promise.all([productsList, categories])
+      .then(([productsList, categories]) => {
+        res.render("productsList", {
+          productsList,
+          categories,
+          toThousand
+        });
+      })
 
   },
   cart: (req, res) => {
@@ -75,54 +75,39 @@ module.exports = {
     // });
   },
   update: (req, res) => {
-    /* console.log(req.body)
-  console.log(req.file) */
     const id = req.params.id;
     const { name, price, gender, description } = req.body;
-    /*     const productsModify = products.map(product => {//map:me crea un nuevo array 
-          if (product.id === +req.params.id) {
-            product.model = name.trim();
-            product.price = +price;
-            product.gender = gender;
-            product.description = description.trim();
-            req.file &&
-              existsSync(`./public/images/products/${product.image}`) &&
-              unlinkSync(`./public/images/products/${product.image}`);
-            product.image = req.file ? req.file.filename : product.image;
-          }
-          return product
-        })
-        writeJSON(productsModify, "./products.json")//que y donde lo guardo
-        res.redirect('/admin') */
-    ///////////////////////////////////////////////
-
+  
     db.Product.findByPk(id)
       .then((product) => {
-        deleteImage(product.image)
-      })
-
-    db.Product.findByPk(id)
-      .then(() => {
-
-        db.Product.update(
+        const previousImage = product.image; 
+        const newImage = req.file ? req.file.filename : previousImage;
+  
+        return db.Product.update(
           {
             model: name.trim(),
             price,
             gender,
-            image: req.file.filename,
-            description: description.trim()
+            image: newImage,
+            description: description.trim(),
           },
           {
             where: {
-              id: id
-            }
-          })
-          .catch((error) => console.log(error))
-          .finally(() => {
-            return res.redirect("/admin")
-          })
-      })
-
+              id: id,
+            },
+          }
+        )
+        .then(() => {
+          if (previousImage !== newImage && previousImage !== '') {
+            deleteImage(previousImage);
+          }
+          return res.redirect("/admin");
+        })
+        .catch((error) => {
+          console.log(error);
+          return res.status(500).send("Error en la actualización");
+        });
+      });
   },
   store: (req, res) => {
     const { name, price, gender, description, size } = req.body;
