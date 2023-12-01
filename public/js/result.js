18a7b0ede9ddcdfdcb084b479a04dba26a8830e8
shezedeth@gmail.com <sheze.gomez@gmail.com>
2023-12-01 15:40:02 -0300

    agregada validacion del search...solo si el campo esta vasio

--- public/js/result.js
+++ public/js/result.js
@@ -2,31 +2,46 @@ const $ = (id) => document.getElementById(id);
 const urlBase = "https://apis.datos.gob.ar/georef/api";
 
 window.onload = async function (e) {
-    /************************* NAME VALIDATIONS **************************/
+  /************************* NAME VALIDATIONS **************************/
 
-    $("keywords").addEventListener("blur", function (e) {
-        switch (true) {
-            case !this.value.trim():
-                this.classList.add("is-invalid");
-                break;
-            case !/^[A-Za-z]+$/.test(this.value):
-                $("msgError-name").innerHTML = "Solo caracteres alfabéticos";
-                this.classList.add("is-invalid");
-                break;
-            case this.value.trim().length < 2:
-                $("msgError-name").innerHTML = "Mínimo dos caracteres";
-                this.classList.add("is-invalid");
-                break;
-            default:
-                $("msgError-name").innerHTML = null;
-                this.classList.remove("is-invalid");
-                break;
-        }
-    });
+  $("formSearch").addEventListener("submit", function (e) {
+    e.preventDefault();
 
-    $("name").addEventListener("focus", function (e) {
-        $("msgError-name").innerHTML = null;
-        this.classList.remove("is-invalid");
-    });
+    const elementsForm = $("formSearch").elements;
+    let error = false;
 
-}
+    for (let i = 0; i < elementsForm.length - 1; i++) {
+      switch (true) {
+        case !elementsForm[i].value.trim():
+          elementsForm[i].classList.add("is-invalid");
+          error = true;
+          break;
+
+        default:
+          !error && this.submit();
+          break;
+      }
+
+    }
+
+  });
+  // switch (true) {
+  //     case !this.value.trim():
+  //         e.preventDefault();
+  //         this.classList.add("is-invalid");
+  //       break;
+  //     case !/^[a-zA-Z0-9-ZÀ-ÿ\s]{1,40}$/.test(this.value):
+  //         e.preventDefault();
+  //         this.classList.add("is-invalid");
+  //       break;
+  //     case this.value.trim().length < 2:
+  //         e.preventDefault();
+  //         this.classList.add("is-invalid");
+  //       break;
+  //     default:
+  //       $("msgError-name").innerHTML = null;
+  //       this.classList.remove("is-invalid");
+  //       this.classList.add("is-valid");
+  //       break;
+  //   }
+};
