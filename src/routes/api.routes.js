f25c5ef9fde5c9e6a201f622305fda5a37747ba1
shezedeth@gmail.com <sheze.gomez@gmail.com>
2023-12-21 17:15:07 -0300

    api de productos agregada

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,15 +1,23 @@
 const { checkEmail } = require("../controllers/APIs/usersApiController");
 const { getCart, addItemToCart, removeItemToCart, deleteItemToCart, clearCart } = require('../controllers/APIs/cartApiController');
 const router = require("express").Router();
+const {index, show,store,update,delete : destroy} = require('../controllers/APIs/productsApiController');
 
 /* API */
 
 router
     .get("/check-email", checkEmail)
+    
     .get('/cart', getCart)
     .post('/cart', addItemToCart)
     .delete('/cart',removeItemToCart)
     .delete('/cart/item', deleteItemToCart)
     .delete('/cart/all',clearCart)
+    
+    .get('/products',index)
+    .get('/products/:id',show)
+    .post('/products',store)
+    .put('/products/:id',update)
+    .delete('/products/:id',destroy)
 
 module.exports = router;
